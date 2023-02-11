aa8318e5d1710ff4b77a06ae178136ce265bf6cd
Robin-0909114 <Winkels.r1@gmail.com>
2023-02-11 18:39:03 +0100

    CHANGE: Added feature that tracks attendance

---
+++ Test_aanmeldingstool/deletetable.py
@@ -0,0 +1,14 @@
+
+import sqlite3
+  
+
+conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database.db') 
+c = conn.cursor()
+  
+# create table named address of customers with
+# 4 columns id,name age and address
+# drop table
+conn.execute("DROP TABLE attendance")
+  
+# close the connection
+conn.close()
