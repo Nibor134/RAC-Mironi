39b038f70c8e0b04bb45b31ff217024a44ec4f69
milan-1061471 <113016348+milan-1061471@users.noreply.github.com>
2023-02-15 11:13:54 +0100

    FIX: Login

--- Test_aanmeldingstool/Test_app.py
+++ Test_aanmeldingstool/Test_app.py
@@ -1,8 +1,8 @@
 import sqlite3
 from flask import Flask
-from flask import render_template, url_for, flash, request, redirect, send_file
-
-from lib.functions import *
+from flask import render_template, url_for, flash, request, redirect, send_file, Response
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from lib.classes import *
 
 # Flask Settings
 LISTEN_ALL = "0.0.0.0"
@@ -13,29 +13,50 @@ FLASK_DEBUG = True
 app = Flask(__name__)
 app.secret_key = 'Hogeschoolrotterdam'
 
-Authed = False
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+    
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database.db')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM Studenten WHERE id = (?)",[user_id])
+    lu = curs.fetchone()
+    if lu is None:
+        return None
+    else:
+        return User(int(lu[0]), lu[1], lu[2])
 
-#Main Route
-@app.route("/")
+#Main Route  
+@app.route("/", methods=['GET','POST'])
 def redirectpage():
     return redirect(url_for("login"))
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    global Authed
-    if request.method == "POST":
-        if request.form.get('LoginButton'):
-            if login_check():
-                Authed = True
-        elif request.form.get('SignupPageButton'):
+    if current_user.is_authenticated:
+        return redirect(url_for('main'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database.db')
+        curs = conn.cursor()
+        curs.execute("SELECT * FROM Studenten WHERE studentnummer = (?)",    [form.studentnummer.data])
+        user = list(curs.fetchone())
+        Us = load_user(user[0])
+        if form.studentnummer.data == Us.studentnummer and form.password.data == Us.password:
+            login_user(Us, remember=form.remember.data)
+            Umail = list({form.studentnummer.data})[0].split('@')[0]
+            flash('Logged in successfully '+Umail)
+            redirect(url_for('main'))
+        else:
+            flash('Login Unsuccessfull.')
+    if request.method == "POST": 
+        if request.form.get('SignupPageButton'):
             return redirect(url_for("signup"))
         elif request.form.get('HomeButton'):
             return redirect(url_for("login"))
 
-    if Authed == False:
-        return render_template("login.html")
-    else:
-        return redirect(url_for("main"))
+    return render_template('login.html',title='Login', form=form)
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -48,11 +69,6 @@ def signup():
 
 @app.route("/main", methods=['GET', 'POST'])
 def main():
-    global Authed
-
-    if Authed == False:
-        return redirect(url_for("login"))
-
     if request.method == "POST":
         if request.form.get('LogoutButton'):
             Authed = False
