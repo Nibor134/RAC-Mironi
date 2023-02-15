e0f69bf5182467d2512839044b63c36e68793f05
Robin-0909114 <Winkels.R1@gmail.com>
2023-02-15 12:41:17 +0100

    CHANGE: Edited login to temporary Users

--- Test_aanmeldingstool/Test_app.py
+++ Test_aanmeldingstool/Test_app.py
@@ -10,53 +10,83 @@ FLASK_IP = LISTEN_ALL
 FLASK_PORT = 81
 FLASK_DEBUG = True
 
+
 app = Flask(__name__)
 app.secret_key = 'Hogeschoolrotterdam'
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
-    
+
+def connect_to_db():
+    conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database2.db')
+    return conn
+
+# Flaskform Login
+class LoginForm(FlaskForm):
+ username = StringField('Username',validators=[DataRequired()])
+ password = PasswordField('Password',validators=[DataRequired()])
+ remember = BooleanField('Remember Me')
+ submit = SubmitField('Login')
+ def validate_username(self, username):
+    conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database2.db')
+    curs = conn.cursor()
+    curs.execute("SELECT username FROM Users where username = (?)",[username.data])
+    valusername = curs.fetchone()
+    if valusername is None:
+      raise ValidationError('This username ID is not registered. Please register before login')
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+         self.id = id
+         self.username = username
+         self.password = password
+         self.authenticated = False
+    def is_active(self):
+         return self.is_active()
+    def is_anonymous(self):
+         return False
+    def is_authenticated(self):
+         return self.authenticated
+    def is_active(self):
+         return True
+    def get_id(self):
+         return self.id
+
 @login_manager.user_loader
 def load_user(user_id):
-    conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database.db')
+    conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database2.db')
     curs = conn.cursor()
-    curs.execute("SELECT * FROM Studenten WHERE id = (?)",[user_id])
+    curs.execute("SELECT * from Users where id = (?)",[user_id])
     lu = curs.fetchone()
     if lu is None:
-        return None
+      return None
     else:
-        return User(int(lu[0]), lu[1], lu[2])
+      return User(int(lu[0]), lu[1], lu[2])
+
+# Login Route
+@app.route("/login", methods=['GET','POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('main'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database2.db')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM Users where username = (?)",    [form.username.data])
+    user = curs.fetchone()
+    Us = load_user(user[0])
+    if form.username.data == Us.username and form.password.data == Us.password:
+        login_user(Us)
+        return redirect(('main'))
+    else:
+        flash('Login Unsuccessfull.')
+  return render_template('login.html',title='Login', form=form)
+
 
 #Main Route  
 @app.route("/", methods=['GET','POST'])
 def redirectpage():
     return redirect(url_for("login"))
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        conn = sqlite3.connect('Test_aanmeldingstool/databases/test_database.db')
-        curs = conn.cursor()
-        curs.execute("SELECT * FROM Studenten WHERE studentnummer = (?)",    [form.studentnummer.data])
-        user = list(curs.fetchone())
-        Us = load_user(user[0])
-        if form.studentnummer.data == Us.studentnummer and form.password.data == Us.password:
-            login_user(Us, remember=form.remember.data)
-            Umail = list({form.studentnummer.data})[0].split('@')[0]
-            flash('Logged in successfully '+Umail)
-            redirect(url_for('main'))
-        else:
-            flash('Login Unsuccessfull.')
-    if request.method == "POST": 
-        if request.form.get('SignupPageButton'):
-            return redirect(url_for("signup"))
-        elif request.form.get('HomeButton'):
-            return redirect(url_for("login"))
-
-    return render_template('login.html',title='Login', form=form)
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -68,15 +98,19 @@ def signup():
 
 
 @app.route("/main", methods=['GET', 'POST'])
+@login_required
 def main():
-    if request.method == "POST":
-        if request.form.get('LogoutButton'):
-            Authed = False
-            return redirect(url_for("login"))
+    #if request.method == "POST":
+        #if request.form.get('LogoutButton'):
+            #Authed = False
+            #return redirect(url_for("login"))
 
     return render_template("main.html")
 
-
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect("login")
 
 if __name__ == "__main__":
     app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
