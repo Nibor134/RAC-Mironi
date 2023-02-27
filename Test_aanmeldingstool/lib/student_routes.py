0487f87788ae4eade130a7b1827d92112b37b4d3
Robin-0909114 <Winkels.r1@gmail.com>
2023-02-27 13:47:24 +0100

    UPDATE:Added Student schedule to student dashboard

--- Test_aanmeldingstool/lib/student_routes.py
+++ Test_aanmeldingstool/lib/student_routes.py
@@ -1,7 +1,9 @@
 # Route to create new class
 from flask import Flask, request, jsonify, Blueprint, session, redirect, render_template, url_for, flash
 from flask_cors import CORS
+import requests
 import sqlite3
+from icalendar import Calendar
 
 student_route = Blueprint('student_route', __name__)
 
@@ -13,3 +15,29 @@ def check_in():
         flash('Log alstublieft eerst in', 'danger')
         return redirect(url_for('login'))
     
+@student_route.route('/rooster')
+def rooster():
+    # Haal de ics file op vanaf de URL
+    url = 'https://roosterapi.hr.nl/timetables/personalized/0909114/export'
+    response = requests.get(url)
+    ics_content = response.text
+    cal_data = response.content.decode('utf-8')
+    # Verwerk de ics file met behulp van icalendar module
+    cal = Calendar.from_ical(cal_data)
+    events = []
+    for event in cal.walk('VEVENT'):
+        event_summary = event.get('summary')
+        event_location = event.get('location')
+        event_description = event.get('description')
+        event_start = event.get('dtstart').dt
+        event_end = event.get('dtend').dt
+        events.append({
+            'summary': event_summary,
+            'location': event_location,
+            'description': event_description,
+            'start': event_start,
+            'end': event_end
+        })
+
+    # Geef de verwerkte ics gegevens door aan de template
+    return render_template('rooster.html', events=events)
